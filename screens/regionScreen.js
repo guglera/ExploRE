8130bacc582c977cd66967c5cf3b856908bb5193
Andreas Eidlinger <ea3793@mci4me.at>
2020-12-20 16:39:39 +0100

    Update regionScreen.js

--- screens/regionScreen.js
+++ screens/regionScreen.js
@@ -10,26 +10,17 @@ function RegionScreen({ navigation }) {
 
   <View style={{ flex: 2}}>
     <Text style={styles.loremIpsum}>
-      My Region - Lernen Sie Ihre Region kennen         
+      Lernen Sie Ihre Region kennen         
   </Text>
   </View>
 
   <View style={{ flex: 1.5}}>
-    <View style={styles.buttons}>
-    <Button
-      color="black"
-      title="Zurück"
-      onPress={() => navigation.goBack()} 
-    />
-    </View>
 
-    <View style={styles.buttons}>
-    <Button
-      color="black"
-      title="Logout"
-      onPress={() => navigation.navigate('ExploRE')} 
-    />
-    </View>
+  <TouchableOpacity
+            onPress={() => navigation.navigate('ExploRE')}
+            View style={styles.buttons}>
+              <Text style={styles.buttonTxt}>Logout</Text>
+          </TouchableOpacity>
 
   </View>
 
@@ -37,6 +28,22 @@ function RegionScreen({ navigation }) {
 </View >
     );
   }
+
+  const colors = {
+    scrollViewBackgrColor: 
+    '#f4f6fc',
+    buttonBackgrColor:
+    //'#fbc176'
+    //'white'
+    //'#1f3e51'
+    //'#4a6676'
+    '#73a0ba'
+    ,
+    buttonTxtColor: '#fff',
+    headlineTxtColor: 'white',
+    headlineTxtBackgrColor: 'rgba(115,160,186,0.65)',
+    containerColor: '#4263ec',
+  };
 
   const styles = StyleSheet.create({
     container: {
@@ -49,23 +56,34 @@ function RegionScreen({ navigation }) {
     loremIpsum: {
       textAlign: "center",
       fontSize: 40,
-      marginTop: 0,
+      marginTop: 30,
+      paddingBottom: 0,
+      color: 'white'
     },
   
     image: {
       flex: 1,
       resizeMode: "cover",
       justifyContent: "center",
-      opacity: 0.70
+      opacity: 1
     },
   
+    buttonTxt: {
+      fontSize: 18,
+      color: colors.buttonTxtColor,
+      textShadowRadius: 10,
+    },
+   
     buttons: {
-      width: '90%',
-      height: 50,
-      backgroundColor: '#2196F3',
-      margin: 20
+      flexDirection: 'row', 
+      justifyContent: 'center',
+      backgroundColor: colors.buttonBackgrColor,
+      marginVertical: 10, marginHorizontal: 16,
+      paddingVertical: 30, paddingHorizontal: 20,
+      borderRadius: 20,
+      elevation: 5,
     }
-  
+
   });
 
 
