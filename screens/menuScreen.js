44c77808d8ad0a7dee40c416006700816303ce8e
Andreas Eidlinger <ea3793@mci4me.at>
2020-12-20 20:52:05 +0100

    Update menuScreen.js

--- screens/menuScreen.js
+++ screens/menuScreen.js
@@ -1,14 +1,19 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Image, ImageBackground, TouchableOpacity, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground, TouchableOpacity, Button, Alert, Dimensions } from 'react-native';
 
+import ImageZoom from 'react-native-image-pan-zoom';
 
 function MenuScreen({ navigation }) {
     return (
       <View style={styles.container}>
-        <Image
-                source={require("../assets/menu.png")}
-                style={styles.image}
-            />
+
+            <ImageZoom cropWidth={Dimensions.get('window').width}
+                       cropHeight={Dimensions.get('window').height}
+                       imageWidth={400}
+                       imageHeight={400}>
+                <Image style={{width:400, height:400}}
+                       source={require("../assets/menu.png")}/>
+            </ImageZoom>
     </View >
     );
   }
