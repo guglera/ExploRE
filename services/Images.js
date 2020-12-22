929614e3ddc2c2bd1d9f0829957cad42932e7c46
AndreasKirchebner <75447407+AndreasKirchebner@users.noreply.github.com>
2020-12-22 21:28:38 +0100

    dynamic background pictures

---
+++ services/Images.js
@@ -0,0 +1,12 @@
+const images = {
+    backgrounds: {
+        central: require('../assets/central-backgpic1.png'),
+        bergfrieden: require('../assets/bergfrieden-backgpic.jpg'),
+        fortress: require('../assets/bergfrieden-backgpic.jpg'),
+        quellenhof: require('../assets/quellenhof-backgpic.jpg'),
+        tuxer: require('../assets/tuxerhof-backgpic.jpg'),
+    }
+
+}
+
+export default images;
