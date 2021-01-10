230d4d043511dd53c1ea5c1bc1aa8f304469d0d2
AndreasKirchebner <75447407+AndreasKirchebner@users.noreply.github.com>
2021-01-10 13:49:33 +0100

    Dynamic Language Selection Weather API

--- components/Weather.js
+++ components/Weather.js
@@ -9,16 +9,13 @@ import DataService from '../services/DataService';
 
 const weather_api_key = '1eefaf10ed0813c788223cdcf71986be'
 const base_weather_url = 'https://api.openweathermap.org/data/2.5/onecall?'
-
-const language = 'en' //set to 'de' for german
-
-
-
+       
 export function Weather (props) {
     const [current_weather, setCurrentWeather] = useState({temp: 0, icon: '', weather_info: ''})
     const [weather_forecast, setWeatherForecast] = useState({temp: 0, icon: '', weather_info: ''})
 
-    const weather_url = `${base_weather_url}lat=${props.lat}&lon=${props.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${weather_api_key}&lang=${language}`
+    const weather_url = `${base_weather_url}lat=${props.lat}&lon=${props.lon}&exclude=minutely,hourly,alerts&units=metric&appid=${weather_api_key}&lang=${props.language}`
+    
 
     //get current & daily weather forecast weather
     //make API request by Latitude and longitude
@@ -30,7 +27,7 @@ export function Weather (props) {
                 const response = await fetch(weather_url)
                 const result = await response.json()    
                 setCurrentWeather({temp : result.current.temp, icon: result.current.weather[0].icon, weather_info: result.current.weather[0].description})
-                setWeatherForecast({temp: result.daily[0].temp.day, icon: result.daily[0].weather[0].icon, weather_info: result.daily[0].weather[0].main})  
+                setWeatherForecast({temp: result.daily[0].temp.day, icon: result.daily[0].weather[0].icon, weather_info: result.daily[0].weather[0].description})  
                 console.log('#debug Weather.js - temp: ' + result.current.temp)        
             } catch (error) {
                 console.error(error)
@@ -38,7 +35,7 @@ export function Weather (props) {
         })();
     }, [props]);
 
-    
+    console.log("#debug Weather.js - Weather Forecast:" + weather_forecast)
    
     const iconUrl = `https://openweathermap.org/img/wn/${current_weather.icon}@4x.png`
     const tomorrow_iconUrl = `https://openweathermap.org/img/wn/${weather_forecast.icon}@4x.png`
