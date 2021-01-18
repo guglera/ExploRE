230be5518d32ad7b96a49ee2fa52f0568baf6ee0
Andreas Eidlinger <ea3793@mci4me.at>
2021-01-18 13:29:18 +0100

    Update Languages.js

--- components/Languages.js
+++ components/Languages.js
@@ -13,7 +13,17 @@ i18n.translations = {
     weatherWelcomeScreen1: 'Current Weather', weatherWelcomeScreen2: 'Weather for tomorrow',
     txtResidenceScreen1: 'ExploRe your residence', txtResidenceScreen2: 'to',
     bttnResidenceScreen1: 'Forward me to the Hotel Website', bttnResidenceScreen2: 'Show me the Morning Brief', bttnResidenceScreen3: 'Show me the Menu', bttnResidenceScreen4: 'Show me residence activities',
-    txtRegionScreen1: 'ExploRe your region', txtRegionScreen2: "there's a lot to see"
+    txtRegionScreen1: 'ExploRe your region', txtRegionScreen2: "there's a lot to see",
+    
+    txtAboutScreen1: 'Data Security', txtAboutScreen2: 'Declaration on the information obligation', 
+    txtAboutScreen3: 'At ExploRe, we take the protection of your personal data very seriously. We treat your personal data confidentially and in accordance with the statutory data protection regulations and this data protection declaration. When you use these apps, various personal data are collected. Personal data are data with which you can be personally identified. This data protection declaration explains which data we collect and what we use it for. It also explains how and for what purpose this is done. We would like to point out that data transmission over the Internet (e.g. when communicating by email) can have security gaps. A complete protection of the data against access by third parties is not possible.', 
+    txtAboutScreen4: 'Processing of data (customer and contract data)',
+    txtAboutScreen5: 'We collect, process and use personal data only insofar as they are necessary for the establishment, content or change of the legal relationship (inventory data). This is done on the basis of Article 6 (1) (b) GDPR, which allows the processing of data to fulfill a contract or pre-contractual measures. We collect, process and use personal data about the use of our apps (usage data) only insofar as this is necessary to enable the user to use the service or to bill.', 
+    txtAboutScreen6: 'Your rights', txtAboutScreen7: 'You have the right to receive information about the origin, recipient and purpose of your stored personal data free of charge at any time. You also have the right to request the correction, blocking or deletion of this data. You can contact us at any time at the address given if you have any further questions about data protection. You also have the right to lodge a complaint with the competent supervisory authority. You also have the right to request that the processing of your personal data be restricted under certain circumstances.', 
+    txtAboutScreen8: 'Revocation of your consent to data processing',  
+    txtAboutScreen9: 'Many data processing operations are only possible with your express consent. You can withdraw your consent at any time. An informal e-mail to us is sufficient. The legality of the data processing carried out before the revocation remains unaffected by the revocation.', 
+    txtAboutScreen10: '',
+    txtAboutScreen11: 'You can contact us at:'
 
 
 
@@ -26,7 +36,17 @@ i18n.translations = {
     weatherWelcomeScreen1: 'Aktuelles Wetter', weatherWelcomeScreen2: 'Wetter Morgen',
     txtResidenceScreen1: 'Erkunde deine Residenz', txtResidenceScreen2: 'bis',
     bttnResidenceScreen1: 'Zur Hotelwebsite', bttnResidenceScreen2: 'Morgenpost', bttnResidenceScreen3: 'Menü', bttnResidenceScreen4: 'Aktivitäten',
-    txtRegionScreen1: 'Erkunde deine Region', txtRegionScreen2: "es gibt viel zu sehen"
+    txtRegionScreen1: 'Erkunde deine Region', txtRegionScreen2: "es gibt viel zu sehen",
+    
+    txtAboutScreen1: 'Datenschutzerklärung', txtAboutScreen2: 'Erklärung zur Informationspflicht', 
+    txtAboutScreen3: 'Wir von ExploRe nehmen den Schutz Ihrer persönlichen Daten sehr ernst. Wir behandeln Ihre personenbezogenen Daten vertraulich und entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser Datenschutzerklärung. Wenn Sie diese Apps benutzen, werden verschiedene personenbezogene Daten erhoben. Personenbezogene Daten sind Daten, mit denen Sie persönlich identifiziert werden können. Die vorliegende Datenschutzerklärung erläutert, welche Daten wir erheben und wofür wir sie nutzen. Sie erläutert auch, wie und zu welchem Zweck das geschieht. Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.', 
+    txtAboutScreen4: 'Verarbeiten von Daten (Kunden- und Vertragsdaten)',
+    txtAboutScreen5: 'Wir erheben, verarbeiten und nutzen personenbezogene Daten nur, soweit sie für die Begründung, inhaltliche Ausgestaltung oder Änderung des Rechtsverhältnisses erforderlich sind (Bestandsdaten). Dies erfolgt auf Grundlage von Art. 6 Abs. 1 lit. b DSGVO, der die Verarbeitung von Daten zur Erfüllung eines Vertrags oder vorvertraglicher Maßnahmen gestattet. Personenbezogene Daten über die Inanspruchnahme unserer Apps (Nutzungsdaten) erheben, verarbeiten und nutzen wir nur, soweit dies erforderlich ist, um dem Nutzer die Inanspruchnahme des Dienstes zu ermöglichen oder abzurechnen.', 
+    txtAboutScreen6: 'Ihre Rechte', txtAboutScreen7: 'Sie haben jederzeit das Recht unentgeltlich Auskunft über Herkunft, Empfänger und Zweck Ihrer gespeicherten personenbezogenen Daten zu erhalten. Sie haben außerdem ein Recht, die Berichtigung, Sperrung oder Löschung dieser Daten zu verlangen. Hierzu sowie zu weiteren Fragen zum Thema Datenschutz können Sie sich jederzeit unter der angegebenen Adresse an uns wenden. Des Weiteren steht Ihnen ein Beschwerderecht bei der zuständigen Aufsichtsbehörde zu. Außerdem haben Sie das Recht, unter bestimmten Umständen die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen.', 
+    txtAboutScreen8: 'Widerruf Ihrer Einwilligung zur Datenverarbeitung',  
+    txtAboutScreen9: 'Viele Datenverarbeitungsvorgänge sind nur mit Ihrer ausdrücklichen Einwilligung möglich. Sie können eine bereits erteilte Einwilligung jederzeit widerrufen. Dazu reicht eine formlose Mitteilung per E-Mail an uns. Die Rechtmäßigkeit der bis zum Widerruf erfolgten Datenverarbeitung bleibt vom Widerruf unberührt.', 
+    txtAboutScreen10: '',
+    txtAboutScreen11: 'Sie erreichen uns unter folgenden Kontaktdaten:'
   },
   };
 
