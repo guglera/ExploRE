3c82203a5e027b574c49f7e3d7a3c4ac603c5010
Andreas Eidlinger <ea3793@mci4me.at>
2021-01-18 13:29:23 +0100

    Update aboutScreen.js

--- screens/aboutScreen.js
+++ screens/aboutScreen.js
@@ -6,6 +6,10 @@ import { useState, useEffect } from 'react';
 import * as asyncStorage from '@react-native-async-storage/async-storage';
 import { ScrollView } from 'react-native-gesture-handler';
 
+import { translations } from '../components/Languages';
+import i18n from 'i18n-js';
+i18n.fallbacks = true;
+
 function AboutScreen({ navigation }) {
   const [value, setValue] = useState(null);
   const { getItem, setItem } = asyncStorage.useAsyncStorage('displaylanguage');
@@ -36,7 +40,11 @@ function AboutScreen({ navigation }) {
       setVisible(!visible);
     };
 
+  //i18n.locale = globalLang.language.displaylanguage;
+
   console.log("#debug aboutScreen.js - displaylanguage: " + value);
+
+
 
   return (
     <View style={styles.container}>
@@ -47,16 +55,30 @@ function AboutScreen({ navigation }) {
 
         <View style={{  flex: 1, paddingTop: 0, paddingBottom: 5, margin: 20, justifyContent: 'center'}}>
           <TouchableOpacity
-            onPress={() => {writeItemToStorage('en');
+            onPress={() => {
+              writeItemToStorage('en');
                             Alert.alert("Language","English language selected");
+                            navigation.reset({
+                              index: 0,
+                              routes: [{
+                                  name: 'About'
+                                },],
+                            });
             }}
             style={styles.buttons}>
               <Text style={styles.buttonTxt}>English</Text>
           </TouchableOpacity>
 
           <TouchableOpacity
-            onPress={() => {writeItemToStorage('de');
+            onPress={() => {
+              writeItemToStorage('de');
                             Alert.alert("Sprache","Deutsche Sprache gewählt");
+                            navigation.reset({
+                              index: 0,
+                              routes: [{
+                                  name: 'About'
+                                },],
+                            });
             }}
             style={styles.buttons}>
               <Text style={styles.buttonTxt}>Deutsch</Text>
@@ -65,7 +87,7 @@ function AboutScreen({ navigation }) {
           <TouchableOpacity
               onPress={toggleOverlay}
               style={styles.buttons}>
-                  <Text style={styles.buttonTxt}>Datenschutzerklärung</Text>
+                  <Text style={styles.buttonTxt}>{i18n.t('txtAboutScreen1')}</Text>
           </TouchableOpacity>
 
         </View>
@@ -79,21 +101,20 @@ function AboutScreen({ navigation }) {
       >
           <View>
             <ScrollView>
-                <Text style={styles.h1text}>Erklärung zur Informationspflicht</Text>
-                <Text style={styles.normaltext}>Wir von ExploRe nehmen den Schutz Ihrer persönlichen Daten sehr ernst. Wir behandeln Ihre personenbezogenen Daten vertraulich und entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser Datenschutzerklärung.
-                        Wenn Sie diese Apps benutzen, werden verschiedene personenbezogene Daten erhoben. Personenbezogene Daten sind Daten, mit denen Sie persönlich identifiziert werden können. Die vorliegende Datenschutzerklärung erläutert, welche Daten wir erheben und wofür wir sie nutzen. Sie erläutert auch, wie und zu welchem Zweck das geschieht.
-                        Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.{"\n"} </Text>
+                <Text style={styles.h1text}>{i18n.t('txtAboutScreen1')}</Text>
+                <Text style={styles.h1text}>{i18n.t('txtAboutScreen2')}</Text>
+                <Text style={styles.normaltext}>{i18n.t('txtAboutScreen3')}{"\n"} </Text>
 
-                <Text style={styles.h1text}>Verarbeiten von Daten (Kunden- und Vertragsdaten)</Text>
-                <Text style={styles.normaltext}>Wir erheben, verarbeiten und nutzen personenbezogene Daten nur, soweit sie für die Begründung, inhaltliche Ausgestaltung oder Änderung des Rechtsverhältnisses erforderlich sind (Bestandsdaten). Dies erfolgt auf Grundlage von Art. 6 Abs. 1 lit. b DSGVO, der die Verarbeitung von Daten zur Erfüllung eines Vertrags oder vorvertraglicher Maßnahmen gestattet. Personenbezogene Daten über die Inanspruchnahme unserer Apps (Nutzungsdaten) erheben, verarbeiten und nutzen wir nur, soweit dies erforderlich ist, um dem Nutzer die Inanspruchnahme des Dienstes zu ermöglichen oder abzurechnen.{"\n"} </Text>
+                <Text style={styles.h1text}>{i18n.t('txtAboutScreen4')}</Text>
+                <Text style={styles.normaltext}>{i18n.t('txtAboutScreen5')}{"\n"} </Text>
 
-                <Text style={styles.h1text}>Ihre Rechte</Text>
-                <Text style={styles.normaltext}>Sie haben jederzeit das Recht unentgeltlich Auskunft über Herkunft, Empfänger und Zweck Ihrer gespeicherten personenbezogenen Daten zu erhalten. Sie haben außerdem ein Recht, die Berichtigung, Sperrung oder Löschung dieser Daten zu verlangen. Hierzu sowie zu weiteren Fragen zum Thema Datenschutz können Sie sich jederzeit unter der angegebenen Adresse an uns wenden. Des Weiteren steht Ihnen ein Beschwerderecht bei der zuständigen Aufsichtsbehörde zu. Außerdem haben Sie das Recht, unter bestimmten Umständen die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen.{"\n"} </Text>
+                <Text style={styles.h1text}>{i18n.t('txtAboutScreen6')}</Text>
+                <Text style={styles.normaltext}>{i18n.t('txtAboutScreen7')}{"\n"} </Text>
 
-                <Text style={styles.h1text}>Widerruf Ihrer Einwilligung zur Datenverarbeitung</Text>
-                <Text style={styles.normaltext}>Viele Datenverarbeitungsvorgänge sind nur mit Ihrer ausdrücklichen Einwilligung möglich. Sie können eine bereits erteilte Einwilligung jederzeit widerrufen. Dazu reicht eine formlose Mitteilung per E-Mail an uns. Die Rechtmäßigkeit der bis zum Widerruf erfolgten Datenverarbeitung bleibt vom Widerruf unberührt.{"\n"} </Text>
+                <Text style={styles.h1text}>{i18n.t('txtAboutScreen8')}</Text>
+                <Text style={styles.normaltext}>{i18n.t('txtAboutScreen9')}{"\n"} </Text>
                                
-                <Text style={styles.h1text}>Sie erreichen uns unter folgenden Kontaktdaten:</Text>
+                <Text style={styles.h1text}>{i18n.t('txtAboutScreen11')}</Text>
                 <Text style={styles.normaltext}>ExploRe{"\n"}Universitätstraße 15 {"\n"}6020 Innsbruck</Text>
             </ScrollView>
           </View>
